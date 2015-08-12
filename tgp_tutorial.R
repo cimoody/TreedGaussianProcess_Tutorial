82df02d9310aa55fdb4b3e5bbf4d06f2dbcac7a0
cimoody <cristina.isadora.moody@gmail.com>
2015-08-12 15:38:14 -0400

    Starting Treed Gaussian Process Tutorial Following http://bobby.gramacy.com/r_packages/tgp/ & http://www.cran.r-project.org/web/packages/tgp/vignettes/tgp.pdf

---
+++ tgp_tutorial.R
@@ -0,0 +1,12 @@
+## Treed Gaussian Process tutorial
+# tgp: an R package
+# Following:
+#    http://bobby.gramacy.com/r_packages/tgp/
+#    http://www.cran.r-project.org/web/packages/tgp/vignettes/tgp.pdf
+# done by Cristina Moody 17 July 2015
+
+install.packages("tgp");
+install.packages(c("akima", "maptree"));
+library(tgp);
+library(akima);
+library(maptree);
